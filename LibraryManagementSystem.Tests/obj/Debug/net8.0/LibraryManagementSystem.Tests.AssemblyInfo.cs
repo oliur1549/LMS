a15f0408c9daa9fa368cb30a5b0852a0e3c9ea95
oliur.khan <oliur.khan@technonext.com>
2026-04-19 20:28:23 +0600

    test gitignore added

--- LibraryManagementSystem.Tests/obj/Debug/net8.0/LibraryManagementSystem.Tests.AssemblyInfo.cs
+++ LibraryManagementSystem.Tests/obj/Debug/net8.0/LibraryManagementSystem.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryManagementSystem.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e33fed17c0dc0bb1abfc8910446179e8e5d6150")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryManagementSystem.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryManagementSystem.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
